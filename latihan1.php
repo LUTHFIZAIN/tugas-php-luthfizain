2ff7cdd609ab0707bfe2f2d9c2348cfc4a74aaff
LUTHFIZAIN <lutfizain740@gmail.com>
2025-06-29 07:45:17 -0700

    modul 2

--- latihan1.php
+++ latihan1.php
@@ -1,10 +1,21 @@
-<?php
-function ganti_style($tulisan, $kelas) {
-    return "<span class=\"$kelas\">$tulisan</span>";
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>modul 2 - latihan 1</title>
+</head>
+<body>
+    <table border="1" cellpadding="3" cellspacing="0">
+        <tr>
+            <th>kolom 1</th>
+            <th>kolom 2</th>
+            <th>kolom 3</th>
+            <th>kolom 4</th>
+            <th>kolom 5</th>
+        </tr>
 
-$tulisan = "Hello World! Here I come!";
-$kelas = "ganti-style";
-
-echo ganti_style($tulisan, $kelas);
-?>
+        <!-- tulis script php dibawah baris ini-->
+    </table>
+</body>
+</html>
